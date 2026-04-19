d0478f053ccd0db93051788d57048aa2245c2aa8
Rajesh Roshan <Rajesh.Roshan@amantyatech.com>
2026-04-19 18:13:12 +0530

    remove deblicate DB connection

--- server.js
+++ server.js
@@ -1,6 +1,5 @@
 import app from './app.js';
 import dotenv from 'dotenv';
-import mongoose from 'mongoose';
 import connectDB from "./config/db.js";
 
 dotenv.config();
@@ -10,7 +9,6 @@ connectDB();
 
 const PORT = process.env.PORT || 5000;
 
-mongoose.connect(process.env.MONGO_URI).then(()=>{
-    console.log('DB Connected');
-    app.listen(PORT, ()=> console.log(`Server running on ${PORT}`))
-}).catch(err=> console.log(err));
+app.listen(PORT, () => {
+  console.log(`Server running on ${PORT}`);
+});

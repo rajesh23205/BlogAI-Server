de4fa5a4d47193717c0cbd93aa85f2f5fc39ab32
Rajesh Roshan <Rajesh.Roshan@amantyatech.com>
2026-04-25 18:16:54 +0530

    connected to pollination ai

--- services/aiService.js
+++ services/aiService.js
@@ -8,10 +8,14 @@ export const suggestPostIdeas = async (topic) => {
   };
 
   export const suggestPostContent = async (topic) => {
-    // call AI API here
-    return [
-      `Top 10 tips about ${topic}`,
-      `Beginner guide to ${topic}`,
-      `Advanced strategies in ${topic}`
-    ];
+    try {
+      const url = `https://gen.pollinations.ai/text/${encodeURIComponent(topic)}?key=${process.env.POLL_API_KEY}`;
+  
+      const response = await fetch(url);
+      const data = await response.text();
+  
+      return data;
+    } catch (error) {
+      console.error("Error:", error);
+    }
   };
